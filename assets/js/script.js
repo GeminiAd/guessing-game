37bd68fe4b08960afaaed2b4a84b2b02b23e7147
Daniel Packer <danielapacker@gmail.com>
2022-09-30 14:31:01 -0700

    add HTML and pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// start button
+//fill-in-the-blank section
+//  user tries to guess letters
+//game timer
+//win = guessed all the letters
+//lose = time runs out
+
+var startButton;
+var fillIn;
+var timer;
+var resultsTracker;
+var playAgain;
+var targetWord;
+
+//We want the user to be able to play a game when they push Start Game
+
+
+//We want time to count down from 10 when the game starts
+//We want the user to be able to guess letters until time runs out
+//We want to keep track of wins and losses over multiple games
+//  local storage
+//We want the user to have the option to play again
